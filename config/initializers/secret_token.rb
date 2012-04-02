aad2e59d886dea44b88ad64069f009d4f04e1f9d
Xing Zhou <dekom.dev@gmail.com>
2012-04-02 01:07:38 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ccr::Application.config.secret_token = 'ca6c29d1d21b3182dc2f220d9cf4c1d3004db06510e577b17c9e17bbabf15a1db9149d15b400b238325e02b1ab138a1852be046d021f9d8fd234436cfd7888c3'
